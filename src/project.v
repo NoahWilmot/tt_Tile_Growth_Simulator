bc67681401d3dd3c674ac7fd6b459044539d03a6
Noah Wilmot <flashw22@gmail.com>
2026-05-05 18:43:31 -0400

    Loading in design

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_tile_growth_simulator_NoahW (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,29 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  wire data;
+
+  tile_growth_simulator TGS (
+    .clock(clk),
+    .reset_n(rst_n),
+    .up(ui_in[0]),
+    .down(ui_in[1]),
+    .left(ui_in[2]),
+    .right(ui_in[3]),
+    .color_sel(ui_in[4]),
+    .place(ui_in[5]),
+    .start(ui_in[6]),
+    .data(data)
+  );
+
+  assign uo_out[0] = data;
+  assign uo_out[7:1] = 7'b000_0000;
+
+  assign uio_out[7:0] = 8'b0000_0000;
+
+  assign uio_oe = 8'b1111_1111;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, uio_in, ui_in[7], 1'b0};
 
 endmodule
